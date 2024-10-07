716ed9772bd92f2901b290ea3d3609d7adce4e4c
alessiofontani <alessiofontani95@gmail.com>
2024-10-07 15:08:13 +0200

    Setup project structure for Vue with Vite and Spring Boot, including Docker and Docker Compose.

---
+++ backend/src/main/java/com/alessiofontani/taskmanagementtool/TaskmanagementtoolApplication.java
@@ -0,0 +1,13 @@
+package com.alessiofontani.taskmanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagementtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagementtoolApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alessiofontani/taskmanagementtool/TaskmanagementtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alessiofontani.taskmanagementtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagementtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
